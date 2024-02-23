61e39b0c2a5eea8dea60f2493216e9832b2b3900
Marwan <funukenichi@gmail.com>
2024-02-23 19:55:05 +0100

    changement branch

---
+++ Qwote/script.js
@@ -0,0 +1,32 @@
+const qwote = document.getElementById("qwote");
+const randomBtn = document.querySelector(".btn");
+const character = document.getElementById("character");
+const anime = document.getElementById("anime");
+const x = document.getElementById("x");
+
+fetchData();
+async function fetchData() {
+	try {
+		const response = await fetch("https://animechan.xyz/api/random");
+		if (!response.ok) {
+			throw new Error("Erreur de récupérations des données");
+		}
+		const data = await response.json();
+		qwote.textContent = data.quote;
+		character.textContent = data.character;
+		anime.textContent = data.anime;
+	} catch (error) {
+		console.log(error);
+	}
+}
+randomBtn.addEventListener("click", () => {
+	fetchData();
+});
+
+x.addEventListener("click", () => {
+	const quote = encodeURIComponent(qwote.textContent);
+	const characterName = encodeURIComponent(character.textContent);
+	const animeName = encodeURIComponent(anime.textContent);
+	const xUrl = `https://twitter.com/intent/tweet?text=${quote} - ${characterName} from ${animeName}`;
+	window.open(xUrl, "_blank");
+});
